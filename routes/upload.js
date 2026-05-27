525037e859cac8276ccc9444d453d2322b5bc34a
boeunparking <boeun.park32@gmail.com>
2026-05-27 11:11:59 +0900

    ADD: dotenv 환경변수 불러오기

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,7 @@
 // backend/routes/upload.js
 import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
